b67a559402cbecfe3f97e2a34ef5b5b4b7e7fd61
Chandra Ramdeen <110476333+ChandraRam18@users.noreply.github.com>
2022-10-30 18:50:35 -0400

    Add files via upload

---
+++ Morse Code Converter/Morse Code Converter/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Morse_Code_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.CovertBTN = new System.Windows.Forms.Button();
+            this.ExitBTN = new System.Windows.Forms.Button();
+            this.OutputLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a string:";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(61, 81);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(354, 160);
+            this.Input.TabIndex = 1;
+            // 
+            // CovertBTN
+            // 
+            this.CovertBTN.Location = new System.Drawing.Point(320, 440);
+            this.CovertBTN.Name = "CovertBTN";
+            this.CovertBTN.Size = new System.Drawing.Size(95, 29);
+            this.CovertBTN.TabIndex = 3;
+            this.CovertBTN.Text = "Convert";
+            this.CovertBTN.UseVisualStyleBackColor = true;
+            this.CovertBTN.Click += new System.EventHandler(this.CovertBTN_Click);
+            // 
+            // ExitBTN
+            // 
+            this.ExitBTN.Location = new System.Drawing.Point(192, 440);
+            this.ExitBTN.Name = "ExitBTN";
+            this.ExitBTN.Size = new System.Drawing.Size(95, 29);
+            this.ExitBTN.TabIndex = 4;
+            this.ExitBTN.Text = "Exit";
+            this.ExitBTN.UseVisualStyleBackColor = true;
+            this.ExitBTN.Click += new System.EventHandler(this.ExitBTN_Click);
+            // 
+            // OutputLB
+            // 
+            this.OutputLB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputLB.Location = new System.Drawing.Point(61, 267);
+            this.OutputLB.Name = "OutputLB";
+            this.OutputLB.Size = new System.Drawing.Size(354, 115);
+            this.OutputLB.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 494);
+            this.Controls.Add(this.OutputLB);
+            this.Controls.Add(this.ExitBTN);
+            this.Controls.Add(this.CovertBTN);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Morse Code Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Button CovertBTN;
+        private System.Windows.Forms.Button ExitBTN;
+        private System.Windows.Forms.Label OutputLB;
+    }
+}
+
---
+++ Morse Code Converter/Morse Code Converter/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morse_Code_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+    private void ExitBTN_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CovertBTN_Click(object sender, EventArgs e)
+        {
+            //create a dictionary to hold all values
+            Dictionary<char, string> mcode = new Dictionary<char, string>()
+            {
+                //Morse Code
+
+                {' ' , " "},
+                {',' , "--..--"},
+                {'.' , ".-.-.-"},
+                {'?' , "..--.."},
+                {'0' , "-----"},
+                {'1' , ".----"},
+                {'2' , "..---"},
+                {'3' , "...--"},
+                {'4' , "....-"},
+                {'5' , "....."},
+                {'6' , "-...."},
+                {'7' , "--..."},
+                {'8' , "---.."},
+                {'9' , "----."},
+                {'A' , ".-"},
+                {'B' , "-..."},
+                {'C' , "-.-."},
+                {'D' , "-.."},
+                {'E' , "."},
+                {'F' , "..-."},
+                {'G' , "--."},
+                {'H' , "...."},
+                {'I' , ".."},
+                {'J' , ".---"},
+                {'K' , "-.-"},
+                {'L' , ".-.."},
+                {'M' , "--"},
+                {'N' , "-."},
+                {'O' , "---"},
+                {'P' , ".--."},
+                {'Q' , "--.-"},
+                {'R' , ".-."},
+                {'S' , "..."},
+                {'T' , "-"},
+                {'U' , "..-"},
+                {'V' , "...-"},
+                {'W' , ".--"},
+                {'X' , "-..-"},
+                {'Y' , "-.--"},
+                {'Z' , "--.."},
+            };
+
+            //Variable to hold user input
+            string UserInput = Input.Text;
+
+            //Convert Input to UpperCase Letters
+            UserInput = UserInput.ToUpper();
+
+            //String used to convert user input to morse code
+            var StringConvert = new StringBuilder();
+
+            //Check each letter of the user input
+            //Ends loop at the end of the UserInput string
+            for (int index = 0; index < UserInput.Length; index++)
+            {
+                var y = UserInput[index];
+                string MorseCodeHolder;
+
+                // TryGetValue gets the value associated with the specific key
+                // In this case, mCode.TryGetValue converts user input
+                // to a variable string ouput, morseHolder.
+
+                if(mcode.TryGetValue(y, out MorseCodeHolder))
+                {
+                    StringConvert.Append(MorseCodeHolder);
+                }
+            }
+
+            //print morse code to the output label
+            OutputLB.Text = StringConvert.ToString();
+        }
+    }
+}
+//Credit goes to: Gabriel codeF7
+//His work has helped me understand what I need to do and
+//know exactly what each command does.
